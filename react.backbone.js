b639d313ba8c6f8404e9980eb0497adf535065ac
Naman Goel <naman34@gmail.com>
2014-04-14 02:17:12 -0700

    Added Commonjs Support.
    Both Backbone and React are easily found on npm and support the commonjs module format.
    By adding a couple of lines react.backbone now works as a commonjs module and can be used with browserify etc.

--- react.backbone.js
+++ react.backbone.js
@@ -1,5 +1,8 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
+    if (typeof exports === 'object') {
+        // CommonJS
+        module.exports = factory(require('backbone'), require('react'));
+    } else if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['backbone', 'react'], factory);
     } else {
